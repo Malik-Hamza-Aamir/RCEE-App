4d2643c635e1b431fea9a04fc7809f4b75e5ddad
Hamza Aamir <hamzaaamir6@gmail.com>
2025-01-13 00:07:33 +0500

    removed extra apps

--- apps/member-service/src/Bootstrap.ts
+++ apps/member-service/src/Bootstrap.ts
@@ -25,6 +25,9 @@ export class Server {
 
   private setupRoutes() {
     this.app.get("/api");
+    this.app.get("/api/health", (req: Request, res: Response) => {
+      res.send({ health: "Member Service is Healthy"})
+    })
 
     this.app.use((req: Request, res: Response, next: NextFunction) => {
       next(new AppError("Route not found", 404));
@@ -40,11 +43,9 @@ export class Server {
   }
 
   public start() {
-    this.app.listen(4000, () => {
-      console.log("PORT :", this.port);
-      
-      console.log("started at port", 4000);
-      logger.info(`Server is running on http://localhost:${4000}`);
+    this.app.listen(this.port, () => {
+      console.log("started at port", this.port);
+      logger.info(`Server is running on http://localhost:${this.port}`);
     });
   }
 }
