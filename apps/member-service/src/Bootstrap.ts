a958c41ffb401e3d14405178ce56a3bceb51b65c
Hamza Aamir <hamzaaamir6@gmail.com>
2025-01-12 22:42:24 +0500

    removed logs added some dependencies

--- apps/member-service/src/Bootstrap.ts
+++ apps/member-service/src/Bootstrap.ts
@@ -3,9 +3,13 @@ import express, { Express, NextFunction, Request, Response } from "express";
 import errorHandler from "./middleware/errorHandler";
 import { AppError } from "./shared/AppError";
 import logger from "./shared/logger";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
 
 export class Server {
   private app: Express;
+  private port = process.env.PORT;
 
   constructor() {
     this.app = express();
@@ -15,6 +19,7 @@ export class Server {
   }
 
   private setupMiddlewares() {
+    this.app.use(cors());
     this.app.use(express.json());
   }
 
@@ -36,6 +41,8 @@ export class Server {
 
   public start() {
     this.app.listen(4000, () => {
+      console.log("PORT :", this.port);
+      
       console.log("started at port", 4000);
       logger.info(`Server is running on http://localhost:${4000}`);
     });
